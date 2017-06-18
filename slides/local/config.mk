e2b11a714487980807813f67e31305370223b7ef
Stefan Evert <stefan.evert@fau.de>
2017-06-18 19:18:08 +0200

    New directory for master copy of presentation slides.

---
+++ slides/local/config.mk
@@ -0,0 +1,10 @@
+##
+##  local configuration of Makefile for LaTeX-Beamer slides
+##
+
+## specify names of presentations here (with extension.pdf)
+FILES := qitl6_evert_arppe.pdf
+
+## directory where generated PDF documents can be installed
+INSTALL_DIR := ../
+
